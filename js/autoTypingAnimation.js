2e0d7c083d065f49e9a42729b980ac240476796f
darshan8890 <darshanpatil05092001@gmail.com>
2023-07-31 23:11:13 +0530

    update

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function() {
+    $('.about-info-inner').typed({
+      strings: [
+        "<span>Hi, I am Darshan Patil</span> a passionate AI enthusiast fascinated by the boundless opportunities presented by practical AI applications. With a curious mind and a thirst for innovation, I immerse myself in the world of artificial intelligence to uncover the transformative possibilities it holds.<br> <br>Embracing the challenges and breakthroughs in AI, I am constantly seeking to expand my knowledge. Through hands-on projects and collaborations, I strive to harness the power of AI to build practical solutions that make a positive impact on society.<br><br>Together, let's shape a future where artificial intelligence enriches lives and transforms the way we live and work."
+      ],
+      typeSpeed: 7,
+      contentType: 'html'
+    });
+  }, 100);
